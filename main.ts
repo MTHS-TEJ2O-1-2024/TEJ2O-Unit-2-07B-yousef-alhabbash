27b1a5a2df8c63d29e25a48a5444b22cfa52057b
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-10-01 12:35:04 -0400

    cookie clicker

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+ /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: Sep 2024
+ * This program will make a cookie clicker
 */
 
-basic.showString('Hello, World!')
+let cookienumber = 0
+basic.showNumber(0)
+
+
+
+basic.clearScreen()
+
+input.onButtonPressed(Button.A, function () {
+
+    cookienumber += 1
+    basic.showNumber(cookienumber)
+
+
+})
+
+    input.onButtonPressed(Button.B, function () {
+
+        cookienumber = 0
+        basic.showNumber(cookienumber)
+
+
+
+
+
+
+})
